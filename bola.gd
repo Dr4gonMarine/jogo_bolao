7f0a3c359a8813b62846a77f55a19e3c6f181561
Dr4gonMarine <65034442+Dr4gonMarine@users.noreply.github.com>
2024-08-16 19:51:08 -0300

    Teste

--- bola.gd
+++ bola.gd
@@ -6,9 +6,13 @@ extends RigidBody3D
 
 @onready var cameraRig = $CameraRig
 @onready var rayCastVerificaChao = $VerificaChao
+@onready var bola: RigidBody3D = $"."
+
+var estaNoChao:bool
+
 
 func _ready() -> void:
-	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	#Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	cameraRig.set_as_top_level(true)
 	rayCastVerificaChao.set_as_top_level(true)
 	
@@ -31,9 +35,25 @@ func _physics_process(delta):
 		elif Input.is_action_pressed("direita"):
 			angular_velocity.z -= VELOCIDADE*delta
 			
-		var estaNoChao = rayCastVerificaChao.is_colliding()
+		estaNoChao = rayCastVerificaChao.is_colliding()
 		
 		if Input.is_action_just_pressed("pular") and estaNoChao:
 			apply_central_impulse(Vector3.UP*IMPULSO_PULO)
 			print("apertou espaço e estaNoChao = true")
-			
+	
+		_gimmick()
+
+
+func transformar_vector3_em_valor(v: Vector3) -> float:
+	# Combina as componentes do Vector3 em um único valor
+	var velocidade = v.length()
+	# Normaliza a magnitude para o intervalo [0, 1]
+	var normalized_magnitude = clamp(velocidade, 0.999, 0.111)
+	# Mapeia o valor para o intervalo de 0.001 a 0.009	
+	return 	normalized_magnitude
+
+
+func _gimmick() -> void:
+	if(bola.scale > Vector3(0.1, 0.1, 0.1) && estaNoChao && angular_velocity != Vector3.ZERO):					
+		bola.scale *= 0.99
+		
