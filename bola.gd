eba6f31befb610865742e40cce62911a67973c5e
Dr4gonMarine <65034442+Dr4gonMarine@users.noreply.github.com>
2024-08-17 01:48:51 -0300

    Novo script de movimentacao e rampa teste

--- bola.gd
+++ bola.gd
@@ -1,19 +1,18 @@
 extends RigidBody3D
 
-@export var VELOCIDADE = 20.0
-@export var IMPULSO_PULO = 500
+@export var VELOCIDADE := 80.0
+@export var IMPULSO_PULO := 500
 @export var PORCENTAGEM_APROXIMACAO_CAMERA = 0.1
+@export var fator_reducao: float = 0.01
 
 @onready var cameraRig = $CameraRig
 @onready var rayCastVerificaChao = $VerificaChao
-@onready var bola: RigidBody3D = $"."
-@onready var collision_shape = bola.get_node("CollisionShape3D")
-@onready var mesh = bola.get_node("MeshInstance3D")  # Assumindo que o mesh está em um nó chamado "MeshInstance3D"
+@onready var mesh: MeshInstance3D = $MeshInstance3D
+@onready var collision_shape: CollisionShape3D = $CollisionShape3D
 
 var estaNoChao: bool
 var tamanho_inicial = 1.0
 var porcentagem_reducao_maxima = 0.9
-var fator_reducao = 0.1
 
 func _ready():
 	cameraRig.set_as_top_level(true)
@@ -27,15 +26,15 @@ func _physics_process(delta):
 	var novaPosicaoBola = lerp(antigaPosicaoCamera, posicaoBola, PORCENTAGEM_APROXIMACAO_CAMERA)
 	cameraRig.global_transform.origin = novaPosicaoBola
 	rayCastVerificaChao.global_transform.origin = global_transform.origin
-
-	if Input.is_action_pressed("frente"):
-		angular_velocity.x -= VELOCIDADE*delta
-	elif Input.is_action_pressed("tras"):
-		angular_velocity.x += VELOCIDADE*delta
-	if Input.is_action_pressed("esquerda"):
-		angular_velocity.z += VELOCIDADE*delta
-	elif Input.is_action_pressed("direita"):
-		angular_velocity.z -= VELOCIDADE*delta
+	
+	var move_direction := Vector3.ZERO	
+	move_direction.x = Input.get_action_strength("tras") - Input.get_action_strength("frente")
+	move_direction.z = Input.get_action_strength("esquerda") - Input.get_action_strength("direita")
+	move_direction = move_direction.rotated(Vector3.UP,cameraRig.rotation.y).normalized()
+	
+	angular_velocity += move_direction * VELOCIDADE * delta	
+	
+	print(angular_velocity)
 
 	estaNoChao = rayCastVerificaChao.is_colliding()
 
