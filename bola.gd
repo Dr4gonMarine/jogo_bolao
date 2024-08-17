c2cfc31f23b3b0b51871bc2e38895a6105ae576d
Pedro dota <galazo0204@gmail.com>
2024-08-16 21:17:56 -0300

    bola redonda

--- bola.gd
+++ bola.gd
@@ -7,53 +7,47 @@ extends RigidBody3D
 @onready var cameraRig = $CameraRig
 @onready var rayCastVerificaChao = $VerificaChao
 @onready var bola: RigidBody3D = $"."
+@onready var collision_shape = bola.get_node("CollisionShape3D")
 
-var estaNoChao:bool
+var estaNoChao: bool
+var tamanho_inicial = 1.0
+var porcentagem_reducao_maxima = 0.6
+var fator_reducao = 0.01
 
-
-func _ready() -> void:
-	#Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+func _ready():
 	cameraRig.set_as_top_level(true)
 	rayCastVerificaChao.set_as_top_level(true)
-	
+	var sphere_shape = collision_shape.shape as SphereShape3D
+	sphere_shape.radius = tamanho_inicial
+
 func _physics_process(delta):
-		#interpolação da movimentação da camera ao seguir a bola
-		var antigaPosicaoCamera = cameraRig.global_transform.origin
-		var posicaoBola = global_transform.origin
-		var novaPosicaoBola = lerp(antigaPosicaoCamera, posicaoBola, PORCENTAGEM_APROXIMACAO_CAMERA)
-		cameraRig.global_transform.origin = novaPosicaoBola
-		
-		#mantém raycast apontando pra baixo independente da rotação da bola
-		rayCastVerificaChao.global_transform.origin = global_transform.origin
-		
-		if Input.is_action_pressed("frente"):
-			angular_velocity.x -= VELOCIDADE*delta
-		elif Input.is_action_pressed("tras"):
-			angular_velocity.x += VELOCIDADE*delta
-		if Input.is_action_pressed("esquerda"):
-			angular_velocity.z += VELOCIDADE*delta
-		elif Input.is_action_pressed("direita"):
-			angular_velocity.z -= VELOCIDADE*delta
-			
-		estaNoChao = rayCastVerificaChao.is_colliding()
-		
-		if Input.is_action_just_pressed("pular") and estaNoChao:
-			apply_central_impulse(Vector3.UP*IMPULSO_PULO)
-			print("apertou espaço e estaNoChao = true")
-	
-		_gimmick()
+	var antigaPosicaoCamera = cameraRig.global_transform.origin
+	var posicaoBola = global_transform.origin
+	var novaPosicaoBola = lerp(antigaPosicaoCamera, posicaoBola, PORCENTAGEM_APROXIMACAO_CAMERA)
+	cameraRig.global_transform.origin = novaPosicaoBola
+	rayCastVerificaChao.global_transform.origin = global_transform.origin
+
+	if Input.is_action_pressed("frente"):
+		angular_velocity.x -= VELOCIDADE*delta
+	elif Input.is_action_pressed("tras"):
+		angular_velocity.x += VELOCIDADE*delta
+	if Input.is_action_pressed("esquerda"):
+		angular_velocity.z += VELOCIDADE*delta
+	elif Input.is_action_pressed("direita"):
+		angular_velocity.z -= VELOCIDADE*delta
+
+	estaNoChao = rayCastVerificaChao.is_colliding()
+
+	if Input.is_action_just_pressed("pular") and estaNoChao:
+		apply_central_impulse(Vector3.UP*IMPULSO_PULO)
+		print("apertou espaço e estaNoChao = true")
 
 
-func transformar_vector3_em_valor(v: Vector3) -> float:
-	# Combina as componentes do Vector3 em um único valor
-	var velocidade = v.length()
-	# Normaliza a magnitude para o intervalo [0, 1]
-	var normalized_magnitude = clamp(velocidade, 0.999, 0.111)
-	# Mapeia o valor para o intervalo de 0.001 a 0.009	
-	return 	normalized_magnitude
-
-
-func _gimmick() -> void:
-	if(bola.scale > Vector3(0.1, 0.1, 0.1) && estaNoChao && angular_velocity != Vector3.ZERO):					
-		bola.scale *= 0.99
-		
+	var velocidade_atual = linear_velocity.length()
+	var sphere_shape = collision_shape.shape as SphereShape3D
+	if velocidade_atual > 0 and sphere_shape.radius > tamanho_inicial * (1 - porcentagem_reducao_maxima):
+		var reducao = velocidade_atual * fator_reducao * delta
+		sphere_shape.radius -= reducao
+		sphere_shape.radius = max(sphere_shape.radius, 0)
+		print("---------------")
+		print(sphere_shape.radius)
