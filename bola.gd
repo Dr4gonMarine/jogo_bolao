3beb5db1b65dd911bed3e362f37f337344eba89a
Dr4gonMarine <65034442+Dr4gonMarine@users.noreply.github.com>
2024-08-17 15:15:21 -0300

    Som bola

--- bola.gd
+++ bola.gd
@@ -9,43 +9,51 @@ extends RigidBody3D
 @onready var rayCastVerificaChao = $VerificaChao
 @onready var mesh: MeshInstance3D = $MeshInstance3D
 @onready var collision_shape: CollisionShape3D = $CollisionShape3D
+@onready var hit_on_ground: AudioStreamPlayer3D = $HitOnGround
+@onready var check_ground: CollisionShape3D = $CheckGround
 
 var estaNoChao: bool
+var estavaNoChao: bool
 var tamanho_inicial = 1.0
 var porcentagem_reducao_maxima = 0.9
+var sphere_shape : SphereShape3D
 
 func _ready():
 	cameraRig.set_as_top_level(true)
 	rayCastVerificaChao.set_as_top_level(true)
-	var sphere_shape = collision_shape.shape as SphereShape3D
+	sphere_shape = collision_shape.shape as SphereShape3D
 	sphere_shape.radius = tamanho_inicial
+	rayCastVerificaChao.target_position.y = -tamanho_inicial
 
 func _physics_process(delta):
 	var antigaPosicaoCamera = cameraRig.global_transform.origin
 	var posicaoBola = global_transform.origin
 	var novaPosicaoBola = lerp(antigaPosicaoCamera, posicaoBola, PORCENTAGEM_APROXIMACAO_CAMERA)
 	cameraRig.global_transform.origin = novaPosicaoBola
-	rayCastVerificaChao.global_transform.origin = global_transform.origin
+	rayCastVerificaChao.position = global_transform.origin
 	
 	var move_direction := Vector3.ZERO	
 	move_direction.x = Input.get_action_strength("tras") - Input.get_action_strength("frente")
 	move_direction.z = Input.get_action_strength("esquerda") - Input.get_action_strength("direita")
 	move_direction = move_direction.rotated(Vector3.UP,cameraRig.rotation.y).normalized()
 	
+	#cameraRig.rotation.y += 
 	angular_velocity += move_direction * VELOCIDADE * delta	
-	
-	print(angular_velocity)
 
 	estaNoChao = rayCastVerificaChao.is_colliding()
+	
+	if(!estavaNoChao and estaNoChao and !hit_on_ground.playing):
+		hit_on_ground.pitch_scale += (1.0 - pow(sphere_shape.radius / tamanho_inicial,0.3))		
+		hit_on_ground.play()
+	estavaNoChao = estaNoChao
 
 	if Input.is_action_just_pressed("pular") and estaNoChao:
 		apply_central_impulse(Vector3.UP*IMPULSO_PULO)
 		print("apertou espaço e estaNoChao = true")
 
-
 	var velocidade_atual = linear_velocity.length()
-	var sphere_shape = collision_shape.shape as SphereShape3D
-	if velocidade_atual > 0 and sphere_shape.radius > tamanho_inicial * (1 - porcentagem_reducao_maxima):
+	sphere_shape = collision_shape.shape as SphereShape3D
+	if velocidade_atual > 0 and sphere_shape.radius > tamanho_inicial * (1 - porcentagem_reducao_maxima) and estaNoChao:
 		var reducao = velocidade_atual * fator_reducao * delta
 		sphere_shape.radius -= reducao
 		sphere_shape.radius = max(sphere_shape.radius, 0)
