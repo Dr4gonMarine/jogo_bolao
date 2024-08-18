32b2c5d0f991ae847b6868906c82288a442838a3
Dr4gonMarine <65034442+Dr4gonMarine@users.noreply.github.com>
2024-08-18 18:02:34 -0300

    Implementação PhantomCamera

--- scenes/coin.gd
+++
@@ -1,8 +0,0 @@
-extends Node3D
-
-@onready var animation_player: AnimationPlayer = $AnimationPlayer
-@onready var coin: Node3D = $"."
-
-func _on_coin_entered(body: Node3D) -> void:
-	if(body.is_in_group("ball")):		
-		animation_player.play("pickup")
